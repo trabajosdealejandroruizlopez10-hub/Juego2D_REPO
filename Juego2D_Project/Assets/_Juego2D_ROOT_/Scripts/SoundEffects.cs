2fc3248ef46ccfd1e971f1a3ef1eed19e4b5e306
MontainDew <hectormontagut29@gmail.com>
2026-01-24 18:03:56 +0100

    efectos de sonido

--- Juego2D_Project/Assets/_Juego2D_ROOT_/Scripts/SoundEffects.cs
+++ Juego2D_Project/Assets/_Juego2D_ROOT_/Scripts/SoundEffects.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SoundByProximity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject player; // El objeto que escuchará el sonido (usualmente el jugador)
+    public AudioSource soundSource; // El AudioSource que emite el sonido
+    public float maxDistance = 20f; // Distancia máxima a la que el sonido se puede escuchar
+    
     void Update()
     {
-        
+        // Calcular la distancia entre el objeto y el jugador
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        // Ajustar el volumen en función de la distancia
+        if (distance <= maxDistance)
+        {
+            // Normaliza el volumen según la distancia
+            float volume = 1 - (distance / maxDistance);
+            soundSource.volume = volume;
+        }
+        else
+        {
+            soundSource.volume = 0;
+        }
+
+        // Si deseas que el sonido empiece o pare en base a la proximidad, puedes usar:
+        if (distance <= maxDistance && !soundSource.isPlaying)
+        {
+            soundSource.Play();
+        }
+        else if (distance > maxDistance && soundSource.isPlaying)
+        {
+            soundSource.Stop();
+        }
     }
 }
