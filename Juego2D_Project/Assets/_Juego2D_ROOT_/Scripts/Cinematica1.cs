5eda09d3b38f541e775aef0b1ffb44e8a7e68ba4
MontainDew <hectormontagut29@gmail.com>
2026-01-25 22:09:00 +0100

    cambios menores

--- Juego2D_Project/Assets/_Juego2D_ROOT_/Scripts/Cinematica1.cs
+++ Juego2D_Project/Assets/_Juego2D_ROOT_/Scripts/Cinematica1.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;  // Necesario para cambiar de escena
 public class SceneController : MonoBehaviour
 {
     public string nextSceneName = "TUTO_FUEGO";
-    public float waitTime = 47f;
+    public float waitTime = 45f;
 
     void Start()
     {
